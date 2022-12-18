dea439b17b1da95b040fc1e6558416c44c6371e5
Kevin Lessard <kevinlessard74@gmail.com>
2022-12-18 03:12:09 -0500

    up

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # echo "TODO: fill in the docker run command"
-docker run -d -p 5000:5000 --env COMET_API_KEY="uSCsJDHCChylrC6vWAR235rC5" -it ift6758/serving:1.0.0
+docker run -d -p 5000:5000 --env COMET_API_KEY=$COMET_API_KEY -it ift6758/serving:1.0.0
